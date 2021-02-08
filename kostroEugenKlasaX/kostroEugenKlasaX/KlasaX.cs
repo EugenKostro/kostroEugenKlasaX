1b22250cb4d172f0426f4c9fdc4c459386680e7b
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2021-02-08 20:33:41 +0100

    Add files via upload

---
+++ kostroEugenKlasaX/kostroEugenKlasaX/KlasaX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenKlasaX
+{
+    class KlasaX
+    {
+        int[] arr = new int[100];
+        int index = 0;
+    }
+}
---
+++ kostroEugenKlasaX/kostroEugenKlasaX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenKlasaX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaX a = new KlasaX();
+            for (int i = 0; i < 100; i++) a.arr[i] = i;
+            a.index = 50;
+            Console.WriteLine(a.arr[a.index]);
+            Console.ReadKey();
+        }
+    }
+}
